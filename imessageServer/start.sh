aa1dbc0bed1baa099391d0fedc689a586e06b4e5
integraloftheday <mason.manetta@gmail.com>
2020-07-21 13:36:11 -0700

    Updated installer.sh and added start.sh

---
+++ imessageServer/start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+
+echo "Starting Server"
+
+source bin/activate 
+
+python app.py
--- installer.sh
+++ installer.sh
@@ -1,13 +1,24 @@
 #!/bin/bash 
 
+echo "Creating Python Virtual Environment"
+
 python3 -m venv PebbleImessageServer
 
 source PebbleImessageServer/bin/activate
 
+echo "Downloading Required Files"
+
 curl -o PebbleImessageServer/app.py https://raw.githubusercontent.com/integraloftheday/Pebble-Imessager/master/imessageServer/app.py
+
+curl -o PebbleImessageServer/config.json https://raw.githubusercontent.com/integraloftheday/Pebble-Imessager/master/imessageServer/config.json
 
 curl -o PebbleImessageServer/requirements.txt https://raw.githubusercontent.com/integraloftheday/Pebble-Imessager/master/imessageServer/requirements.txt
 
 pip install -r PebbleImessageServer/requirements.txt
+
+echo "Now edit config.json to add contacts, to change the port number, and to set the key" 
+
+echo "Use sh start.sh to start the server"
+
 
 
