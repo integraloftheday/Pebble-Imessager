efe62d3bacd4bb3bb24a8caba062e98d9a0ee059
integraloftheday <mason.manetta@gmail.com>
2020-07-25 01:17:27 -0700

    Added Canned Messages ability

--- imessageServer/app.py
+++ imessageServer/app.py
@@ -20,7 +20,7 @@ def sendMsg():
     config = getConfig() 
     res =""
     postData = request.json
-    print(postData)
+    
     '''postData format:
     {
         "msg":"Some String",
@@ -29,10 +29,8 @@ def sendMsg():
     } 
     '''
     if(postData["key"] != config["key"]):
-        res = {
-            "success":"false"
-        }
-        return(res,400)
+        print(postData)
+        return({"success":"false"},400)
 
     postData["msg"] = postData["msg"].replace("'","") # need to find a better way to acount for ' like it's 
     msgCommand = "osascript -e \'tell application \"Messages\" to send \"{0}\" to buddy \"{1}\"\'".format(postData["msg"],postData["to"])
@@ -44,22 +42,28 @@ def sendMsg():
         }
         return(res,200)
     except: 
-        res = {
-            "success":"false"
-        }
-        return(res,400)
-    #cmdOut = os.popen(msgCommand).read()
-    #result = subprocess.run([msgCommand], stdout=subprocess.PIPE)
-    #cmdOut = result.stdout.decode('utf-8')
+        print(postData)
+        return({"success":"false"},400)
 
 @app.route("/msg/api/v1/contacts", methods=['POST'])
 def getContacts():
     config = getConfig()
     postData = request.json
-    print(postData)
+    
     if(postData["key"] == config["key"]):
         return({"success":"true","contacts":config['contacts']},200)
     else:
+        print(postData)
+        return({"success":"false"},400)
+
+@app.route("/msg/api/v1/replies", methods=['POST'])
+def getReplies():
+    config = getConfig() 
+    postData = request.json
+    if(postData["key"] == config["key"]):
+        return({"sucess":"true","quickReplies":config["quickReplies"]},200)
+    else:
+        print(postData)
         return({"success":"false"},400)
 
 @app.route("/msg/api/exception")
@@ -71,6 +75,8 @@ def server_error(e):
     #logging.exception('An error occurred during a request. %s', e)
     return "An internal error occured", 500
 
+
+print("Starting Server")
 http_server = WSGIServer(('', getConfig()["port"]), app)
 http_server.serve_forever()
 
